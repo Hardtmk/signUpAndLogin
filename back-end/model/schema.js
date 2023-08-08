747dac4ca5447d2b31ace80f296d0f30d902f0a8
miuTong <jennymktong@dimbuy.com>
2023-08-08 18:37:16 +0800

    feat: say hi

--- back-end/model/schema.js
+++ back-end/model/schema.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const LoginSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: [true, 'must provide name'],
+    required: [true, "must provide name"],
     trim: true,
-    maxlength: [20, 'name can not be more than 20 characters'],
+    maxlength: [20, "name can not be more than 20 characters"],
   },
   password: {
     type: String,
   },
 });
 // schema就是等於一個model
-module.exports = mongoose.model('schema', LoginSchema);
+module.exports = mongoose.model("schema", LoginSchema);
+console.log("hi");
